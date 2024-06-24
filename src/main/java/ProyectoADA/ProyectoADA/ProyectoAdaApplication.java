e2a731af3d391ac276058c7fb3a01da34e9b02b3
Luis Francisco Marroquin Ortiz <lfortiz.agt@esieduc.org>
2024-06-24 13:55:07 -0600

    Primer commit

---
+++ src/main/java/ProyectoADA/ProyectoADA/ProyectoAdaApplication.java
@@ -0,0 +1,13 @@
+package ProyectoADA.ProyectoADA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAdaApplication.class, args);
+	}
+
+}
